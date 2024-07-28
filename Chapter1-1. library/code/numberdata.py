4dd41d3588c594daa4a1792facd9e821854d99b9
sj hwang <150766879+justinbrianhwang@users.noreply.github.com>
2024-07-28 11:17:07 +0900

    Update numberdata.py

--- Chapter1-1. library/code/numberdata.py
+++ Chapter1-1. library/code/numberdata.py
@@ -1,58 +1,38 @@
 ### numberdata.py
 
-## 숫자데이터 처리 라이브러리
+## Numeric Data Processing Libraries
 
 def p(str):
     print(str, "\n")
 
 # math
 import math
-p(math.gcd(60, 80, 100)) # 최대공약수
-p(math.lcm(15, 25)) # 최소공배수
+p(math.gcd(60, 80, 100))  # Greatest common divisor
+p(math.lcm(15, 25))  # Least common multiple
 
 # decimal
 from decimal import Decimal
-p(0.1 * 3) # 메모리의 한계로 인해서 소수점 연산 부정확함
-p(Decimal('0.1') * 3) # 정확한 소수연산시에 Decimal 사용
+p(0.1 * 3)  # Inaccurate decimal arithmetic due to memory limitations
+p(Decimal('0.1') * 3)  # Accurate decimal arithmetic using Decimal
 
 # fractions
 from fractions import Fraction
-p(Fraction(1.5)) # 분수
+p(Fraction(1.5))  # Fraction
 
 # random
 import random
-p(random.randint(1, 45)) # 1~45까지의 랜덤 정수
+p(random.randint(1, 45))  # Random integer between 1 and 45
 
-# 로또 숫자 6개 추출
+# Extracting 6 random Lotto numbers
 lottoNum = set()
 while True:
     lottoNum.add(random.randint(1, 45))
-    if (len(lottoNum)==6):
+    if len(lottoNum) == 6:
         break
 p(list(lottoNum))
 
 # statistics
 import statistics
 score = [38, 54, 45, 87, 92]
-p(statistics.mean(score)) # 평균값
-p(statistics.median(score)) # 중간값 (=중앙값)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+p(statistics.mean(score))  # Mean
+p(statistics.median(score))  # Median
